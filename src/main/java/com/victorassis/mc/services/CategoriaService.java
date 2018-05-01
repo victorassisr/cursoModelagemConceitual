53c31a53a9793ac53aa9cb72840ca32f06cd81e6
= <victorassis545@gmail.com>
2018-05-01 14:19:38 -0300

    Criando Repository e Service para a Categoria

---
+++ src/main/java/com/victorassis/mc/services/CategoriaService.java
@@ -0,0 +1,19 @@
+package com.victorassis.mc.services;
+
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.victorassis.mc.domain.Categoria;
+import com.victorassis.mc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
